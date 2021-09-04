ae5e06a2921c7c40f32695265cce46e3121debca
PramitaPandit <78577352+PramitaPandit@users.noreply.github.com>
2021-09-04 11:15:14 -0500

    Update GuessThenNumber.py

--- GuessThenNumber.py
+++ GuessThenNumber.py
@@ -1,3 +1,9 @@
 import random 
 n = random.randint(1,9)
-guess = 
+guess = int(input('Guess a number within 1 to 9:'))
+print(f'The number was',n)
+if guess == n:
+    print('Number matched')
+else:
+    print('Try again')
+    
